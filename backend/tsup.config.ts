932c388055a9d021ae734bb821d978f9ac328327
Matheus Santos Araújo <wenked.matheus@yahoo.com.br>
2024-08-08 11:15:19 -0300

    ajustes docker

--- backend/tsup.config.ts
+++ backend/tsup.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/**/*.ts'],
   outDir: 'build',
+  format: ['cjs'],
   splitting: false,
   sourcemap: true,
   clean: true,
