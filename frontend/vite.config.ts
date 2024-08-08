5aeeac15602d569e8eb6412fed588b444e46c041
Matheus Santos Araújo <wenked.matheus@yahoo.com.br>
2024-08-08 11:49:14 -0300

    ajustes nginx

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,10 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+
+const env = loadEnv('all', process.cwd());
+
+const port = Number(env.VITE_PORT) || 5177;
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,4 +12,8 @@ export default defineConfig({
     outDir: 'build',
   },
   plugins: [react(), tsconfigPaths()],
+  server: {
+    port,
+    host: 'localhost',
+  },
 });
